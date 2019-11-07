7ef6d548c600c269c9085e7bf9ca9c0880871aa1
andrav24 <andrav24@yandex.ru>
2019-11-07 21:45:59 +0700

    Import libraries, implement the Model, implement the ViewModel

--- app/src/main/java/tech/andrav/countriesapp/viewmodel/ListViewModel.java
+++ app/src/main/java/tech/andrav/countriesapp/viewmodel/ListViewModel.java
@@ -1,4 +1,38 @@
 package tech.andrav.countriesapp.viewmodel;
 
-public class ListViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tech.andrav.countriesapp.model.CountryModel;
+
+public class ListViewModel extends ViewModel {
+
+    public MutableLiveData<List<CountryModel>> countries = new MutableLiveData<>();
+    public MutableLiveData<Boolean> countryLoadError = new MutableLiveData<>();
+    public MutableLiveData<Boolean> loading = new MutableLiveData<>();
+
+    /**
+     * This method is entry point for the View in our ViewModel
+     */
+    public void refresh() {
+        fetchCounties();
+    }
+
+    private void fetchCounties() {
+        CountryModel country1 = new CountryModel("Albania", "Tirana", "");
+        CountryModel country2 = new CountryModel("Brazil", "Brasilia", "");
+        CountryModel country3 = new CountryModel("Czech", "Praha", "");
+
+        List<CountryModel> list = new ArrayList<>();
+        list.add(country1);
+        list.add(country2);
+        list.add(country3);
+
+        countries.setValue(list);
+        countryLoadError.setValue(false);
+        loading.setValue(false);
+    }
 }
