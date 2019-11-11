3b7270dee35a2aca8c18d142b71a8ed59a116b53
andrav24 <andrav24@yandex.ru>
2019-11-11 22:44:51 +0700

    Get data from the backend

--- app/src/main/java/tech/andrav/countriesapp/viewmodel/ListViewModel.java
+++ app/src/main/java/tech/andrav/countriesapp/viewmodel/ListViewModel.java
@@ -6,6 +6,11 @@ import androidx.lifecycle.ViewModel;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+import tech.andrav.countriesapp.model.CountriesService;
 import tech.andrav.countriesapp.model.CountryModel;
 
 public class ListViewModel extends ViewModel {
@@ -13,6 +18,11 @@ public class ListViewModel extends ViewModel {
     public MutableLiveData<List<CountryModel>> countries = new MutableLiveData<>();
     public MutableLiveData<Boolean> countryLoadError = new MutableLiveData<>();
     public MutableLiveData<Boolean> loading = new MutableLiveData<>();
+
+    private CountriesService countriesService = CountriesService.getInstance();
+
+    //RxJava
+    private CompositeDisposable disposable = new CompositeDisposable();
 
     /**
      * This method is entry point for the View in our ViewModel
@@ -23,38 +33,38 @@ public class ListViewModel extends ViewModel {
     }
 
     private void fetchCountries() {
-        CountryModel country1 = new CountryModel("Albania", "Tirana", "");
-        CountryModel country2 = new CountryModel("Brazil", "Brasilia", "");
-        CountryModel country3 = new CountryModel("Czech", "Praha", "");
+        loading.setValue(true);
+        disposable.add(
+                countriesService.getCountries()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableSingleObserver<List<CountryModel>>() {
 
-        List<CountryModel> list = new ArrayList<>();
-        list.add(country1);
-        list.add(country2);
-        list.add(country3);
-        list.add(country1);
-        list.add(country2);
-        list.add(country3);
-        list.add(country1);
-        list.add(country2);
-        list.add(country3);
-        list.add(country1);
-        list.add(country2);
-        list.add(country3);
-        list.add(country1);
-        list.add(country2);
-        list.add(country3);
-        list.add(country1);
-        list.add(country2);
-        list.add(country3);
-        list.add(country1);
-        list.add(country2);
-        list.add(country3);
-        list.add(country1);
-        list.add(country2);
-        list.add(country3);
+                    @Override
+                    public void onSuccess(List<CountryModel> countryModels) {
+                        countries.setValue(countryModels);
+                        countryLoadError.setValue(false);
+                        loading.setValue(false);
+                    }
 
-        countries.setValue(list);
-        countryLoadError.setValue(false);
-        loading.setValue(false);
+                    @Override
+                    public void onError(Throwable e) {
+                        countryLoadError.setValue(true);
+                        loading.setValue(false);
+                        e.printStackTrace();
+                    }
+                })
+        );
+
+    }
+
+    /**
+     * Если система killed the app этот метод убьет поток в котором было запущено ...
+     * то, что была в disposable и у нас не будет утечек памяти.
+     */
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        disposable.clear();
     }
 }
